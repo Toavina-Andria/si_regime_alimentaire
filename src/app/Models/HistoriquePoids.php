993c32c3960e35b77cfed91b5cda6c5bce6e208e
Toavina <teoandriamonta@gmail.com>
2026-05-06 11:50:20 +0300

    Modif des historiques de poids

--- src/app/Models/HistoriquePoids.php
+++ src/app/Models/HistoriquePoids.php
@@ -6,41 +6,24 @@ use CodeIgniter\Model;
 
 class HistoriquePoids extends Model
 {
-    protected $table            = 'historiquepoids';
+    protected $table            = 'historique_poids';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['utilisateur_id', 'poids_kg', 'note', 'mesure_le'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
 
-    protected array $casts = [];
-    protected array $castHandlers = [];
-
-    // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
 
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    // Relationships
+    public function utilisateur()
+    {
+        return $this->belongsTo(Utilisateur::class, 'utilisateur_id', 'id');
+    }
 }
