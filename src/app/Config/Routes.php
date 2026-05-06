142e128241b180333df8af4410f8d2c2206053e9
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-05-06 12:06:58 +0300

    dash route

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -5,4 +5,14 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'DashboardController::index');
+$routes->get('/code', 'CodeController::index');
+$routes->post('/code/verify', 'CodeController::verifier');
+// La page d'accueil affiche le login
+$routes->get('/', 'Auth::index');
+
+// Route pour afficher le formulaire de profil
+$routes->get('auth/profil', 'Auth::profil');
+
+// Route pour traiter l'envoi du login et rediriger
+$routes->post('auth/register', 'Auth::register');
