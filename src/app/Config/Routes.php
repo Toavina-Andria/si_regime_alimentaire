eaf1bc03d94a48300e7e027cdee5c00a634d753e
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-05-06 11:55:08 +0300

    code route

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'DashboardController::index');
+$routes->get('/code', 'CodeController::index');
+$routes->post('/code/verify', 'CodeController::verifier');
