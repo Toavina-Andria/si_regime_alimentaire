a3c86faadeb5158007074e1a5b2a43bfc77ed709
Toavina <teoandriamonta@gmail.com>
2026-05-06 11:51:16 +0300

    Correction du nom de la table et ajout des champs autorisés dans le modèle UtilisateurAbonnement

--- src/app/Models/UtilisateurAbonnement.php
+++ src/app/Models/UtilisateurAbonnement.php
@@ -6,41 +6,29 @@ use CodeIgniter\Model;
 
 class UtilisateurAbonnement extends Model
 {
-    protected $table            = 'utilisateurabonnements';
+    protected $table            = 'utilisateur_abonnement';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['utilisateur_id', 'abonnement_id', 'date_debut', 'date_fin', 'statut'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
 
-    protected array $casts = [];
-    protected array $castHandlers = [];
-
-    // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
 
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
+    // Relationships
+    public function utilisateur()
+    {
+        return $this->belongsTo(Utilisateur::class, 'utilisateur_id', 'id');
+    }
 
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    public function abonnement()
+    {
+        return $this->belongsTo(Abonnement::class, 'abonnement_id', 'id');
+    }
 }
