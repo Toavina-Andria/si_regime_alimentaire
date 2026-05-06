c1a910b6f40ebe5befd920134de46798633c1ecc
Toavina <teoandriamonta@gmail.com>
2026-05-06 11:50:44 +0300

    Correction du nom de la table et ajout des champs autorisés dans le modèle RegimePrix

--- src/app/Models/RegimePrix.php
+++ src/app/Models/RegimePrix.php
@@ -6,41 +6,28 @@ use CodeIgniter\Model;
 
 class RegimePrix extends Model
 {
-    protected $table            = 'regimeprixes';
+    protected $table            = 'regime_prix';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['regime_id', 'duree_jours', 'prix_base'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
 
-    protected array $casts = [];
-    protected array $castHandlers = [];
-
-    // Dates
     protected $useTimestamps = false;
     protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
 
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
+    // Relationships
+    public function regime()
+    {
+        return $this->belongsTo(Regime::class, 'regime_id', 'id');
+    }
 
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    public function souscriptions()
+    {
+        return $this->hasMany(SouscriptionRegime::class, 'regime_prix_id', 'id');
+    }
 }
