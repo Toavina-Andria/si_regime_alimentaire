a294dff51039616b0ca2032eece418a6baf15f6b
Celina-D26 <celinarakotobe@gmail.com>
2026-05-07 19:27:19 +0300

    Profil

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -5,18 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-// La page d'accueil affiche le login $routes->get('/', 'Auth::index');
-
-
-
-
 $routes->get('/', 'Auth::index');
 $routes->post('auth/register', 'Auth::register');
 $routes->get('auth/profil', 'Auth::profil');
 $routes->post('auth/updateProfil', 'Auth::updateProfil');
 $routes->get('dashboard', 'Auth::dashboard');
-$routes->get('auth/logout', 'Auth::logout');
-
-// Optionnel : page de connexion pour les déjà inscrits (à créer si besoin)
-// $routes->get('connexion', 'Auth::loginForm');
-// $routes->post('auth/doLogin', 'Auth::doLogin');
+$routes->get('logout', 'Auth::logout');
