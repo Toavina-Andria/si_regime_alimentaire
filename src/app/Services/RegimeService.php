e1644404c5ce7c9e9c2e33a55a8a77713e532241
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-05-09 01:36:31 +0300

    validateur de point

--- src/app/Services/RegimeService.php
+++ src/app/Services/RegimeService.php
@@ -4,13 +4,23 @@ namespace App\Services;
 use App\Models\Regime;
 use App\Models\RegimePrix;
 use App\Models\RegimeActivite;
+use App\Models\Abonnement;
+use App\Models\UtilisateurAbonnement;
+use App\Models\SouscriptionRegime;
 
 class RegimeService
 {
+    private static $dateFormat = 'Y-m-d H:i:s';
     public static function getRegimePrixByRegimeId($regimeId)
     {
         $regimePrixModel = new RegimePrix();
         return $regimePrixModel->where('regime_id', $regimeId)->findAll();
     }
 
+    // validate user solde points
+    public static function validateUserPoints($userId, $prix)
+    {        $portefeuilleModel = UtilisateurService::getPortefeuilleByUserId($userId);
+        $soldePoints = $portefeuilleModel['solde_points'];
+        return $soldePoints >= $prix;
+    }
 }
