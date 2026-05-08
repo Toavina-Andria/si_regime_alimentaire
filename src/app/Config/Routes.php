b561bc1b09a18790fc832f9cbbf5e389a7c01382
Toavina <teoandriamonta@gmail.com>
2026-05-08 09:20:20 +0300

    Ajout des Routes

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -12,10 +12,10 @@ $routes->post('auth/updateProfil', 'Auth::updateProfil');
 $routes->get('dashboard', 'Auth::dashboard');
 $routes->get('logout', 'Auth::logout');
 
-
 // Routes pour les régimes alimentaires
 $routes->get('regimes', 'RegimeController::index');
 $routes->get('regime/(:num)', 'RegimeController::show/$1');
+
 // Route pour traiter l'envoi du login et rediriger
 $routes->post('auth/register', 'Auth::register');
 $routes->get('/', 'Auth::index');
@@ -26,3 +26,12 @@ $routes->get('dashboard', 'Auth::dashboard');
 $routes->get('connexion', 'Auth::loginForm');
 $routes->post('auth/doLogin', 'Auth::doLogin');
 $routes->get('logout', 'Auth::logout');
+
+// Routes Admin / Dashboard
+$routes->group('admin', static function ($routes) {
+    $routes->get('dashboard', 'DashboardController::index');
+    $routes->get('regimes', 'DashboardController::regimes');
+    $routes->get('codes', 'DashboardController::codes');
+    $routes->get('activites', 'DashboardController::activites');
+    $routes->get('utilisateurs', 'DashboardController::utilisateurs');
+});
