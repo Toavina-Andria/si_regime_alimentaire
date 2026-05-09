ab7b8da064f7c4549f6710eb1bc81064b9c9b643
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-05-09 14:50:44 +0300

    register

--- src/app/Services/AuthService.php
+++ src/app/Services/AuthService.php
@@ -15,6 +15,23 @@ class AuthService
 
         return null;
     }
+    public static function register($data)
+    {
+        try {
+             // create user - validation should be done in controller
+            $data = [
+                'nom' => $data['nom'],
+                'prenom' => $data['prenom'],
+                'email' => $data['email'],
+                'mot_de_passe' => $data['mot_de_passe'] // model va hacher
+            ];
+            $util = new Utilisateur();
+            $util->insert($data);
+            return $util->getInsertID();
+        } catch (\Throwable $th) {
+            return redirect()->back()->withInput()->with('error', 'Erreur lors de l\'inscription : ' . $th->getMessage());
+        }
+    }
     public static function logout()
     {
         session()->destroy();
