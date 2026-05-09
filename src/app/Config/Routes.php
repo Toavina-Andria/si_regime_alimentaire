954d07f58725e5f421256ad85ab07f462d73af6b
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-05-10 00:25:57 +0300

    user dashboard and admindashboad separeted

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -33,7 +33,6 @@ $routes->get('regime/(:num)', 'RegimeController::show/$1');
 // Route pour traiter l'envoi du login et rediriger
 $routes->get('auth/profil', 'Auth::profil');
 $routes->post('auth/updateProfil', 'Auth::updateProfil');
-$routes->get('dashboard', 'UserDashboard::index');
 $routes->get('connexion', 'Auth::loginForm');
 $routes->post('auth/doLogin', 'Auth::doLogin');
 $routes->get('logout', 'Auth::logout');
