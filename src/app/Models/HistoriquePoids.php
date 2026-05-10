d2d1aea8d3a357a3731e4e8e7da4c8bd534c9fbe
Toavina <teoandriamonta@gmail.com>
2026-05-10 19:45:25 +0300

    Update des hirtoirques ajouter

--- src/app/Models/HistoriquePoids.php
+++ src/app/Models/HistoriquePoids.php
@@ -20,6 +20,7 @@ class HistoriquePoids extends Model
     protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
+    protected $updatedField  = '';
 
     // Relationships
     public function utilisateur()
