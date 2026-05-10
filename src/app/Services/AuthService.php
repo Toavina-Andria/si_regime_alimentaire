4ea7d807b8900a0adb719ed453c0d9d4eaa4e1c9
Toavina <teoandriamonta@gmail.com>
2026-05-10 23:42:34 +0300

    Final issue de Aiky amle hash

--- src/app/Services/AuthService.php
+++ src/app/Services/AuthService.php
@@ -17,20 +17,17 @@ class AuthService
     }
     public static function register($data)
     {
-        try {
-             // create user - validation should be done in controller
-            $data = [
-                'nom' => $data['nom'],
-                'prenom' => $data['prenom'],
-                'email' => $data['email'],
-                'mot_de_passe' => $data['mot_de_passe'] // model va hacher
-            ];
-            $util = new Utilisateur();
-            $util->insert($data);
-            return $util->getInsertID();
-        } catch (\Throwable $th) {
-            return redirect()->back()->withInput()->with('error', 'Erreur lors de l\'inscription : ' . $th->getMessage());
+        $data = [
+            'nom' => $data['nom'],
+            'prenom' => $data['prenom'],
+            'email' => $data['email'],
+            'mot_de_passe' => $data['mot_de_passe']
+        ];
+        $util = new Utilisateur();
+        if (!$util->insert($data)) {
+            throw new \Exception(implode(', ', $util->errors()));
         }
+        return $util->getInsertID();
     }
     public static function attemptLogin(string $email, string $password)
     {
